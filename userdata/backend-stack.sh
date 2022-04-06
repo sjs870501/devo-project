f609d44068dc3272485d34d7aef9d33cb7a86199
Jae Shin <sjs870501@gmail.com>
2022-04-06 23:59:33 +1000

    backend userdata tried to be changed

--- userdata/backend-stack.sh
+++ userdata/backend-stack.sh
@@ -6,7 +6,7 @@ yum install mariadb-server -y
 yum install wget git unzip -y
 
 #mysql_secure_installation
-sed -i 's/^127.0.0.1/0.0.0.0/' /etc/my.cnf
+#sed -i 's/^127.0.0.1/0.0.0.0/' /etc/my.cnf
 
 # starting & enabling mariadb-server
 systemctl start mariadb
